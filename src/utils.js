bde42e33f1a44b034bc9d4d6c13288915d8276c9
lykmapipo <lallyelias87@gmail.com>
2020-09-07 23:45:51 +0300

    feat(utils): generate wwis link for a city

--- src/utils.js
+++ src/utils.js
@@ -58,3 +58,34 @@ export const findCity = (optns) => {
   // return found city
   return city;
 };
+
+/**
+ * @function wwisLinkFor
+ * @name wwisLinkFor
+ * @description Generate world weather information service for a given city
+ * @param {object} optns Valid options
+ * @param {object} optns.name Valid city name
+ * @returns {string|undefined} city link or undefined
+ * @author lally elias <lallyelias87@mail.com>
+ * @license MIT
+ * @since 0.1.0
+ * @version 0.1.0
+ * @static
+ * @public
+ * @example
+ *
+ * wwisLinkFor({ name : 'Dar Es Salaam' });
+ * // => https://worldweather.wmo.int/en/json/252_en.json
+ */
+export const wwisLinkFor = (optns) => {
+  // find a city
+  const { CityId } = findCity(optns) || {};
+
+  // ignore
+  if (isNotValue(CityId)) {
+    return undefined;
+  }
+
+  // return wwis link
+  return `https://worldweather.wmo.int/en/json/${CityId}_en.json`;
+};
--- test/utils.spec.js
+++ test/utils.spec.js
@@ -1,6 +1,6 @@
 import { expect } from '@lykmapipo/test-helpers';
 
-import { DEFAULT_REQUEST_HEADERS, findCity } from '../src/utils';
+import { DEFAULT_REQUEST_HEADERS, findCity, wwisLinkFor } from '../src/utils';
 
 describe('utils', () => {
   it('should provide default request headers', () => {
@@ -23,6 +23,22 @@ describe('utils', () => {
     expect(findCity({ name: '252' })).to.be.exist.and.be.an('object');
     expect(findCity({ name: 'Dar Es Salaam' })).to.be.eql(
       findCity({ name: '252' })
+    );
+  });
+
+  it('should generate wwis link for a given city', () => {
+    expect(wwisLinkFor()).to.be.undefined;
+    expect(wwisLinkFor({})).to.be.undefined;
+    expect(wwisLinkFor({ name: '' })).to.be.undefined;
+    expect(wwisLinkFor({ name: null })).to.be.undefined;
+    expect(wwisLinkFor({ name: undefined })).to.be.undefined;
+    expect(wwisLinkFor({ name: 'Unknown' })).to.be.undefined;
+    expect(wwisLinkFor({ name: 'Dar Es Salaam' })).to.be.exist.and.be.an(
+      'string'
+    );
+    expect(wwisLinkFor({ name: '252' })).to.be.exist.and.be.an('string');
+    expect(wwisLinkFor({ name: 'Dar Es Salaam' })).to.be.eql(
+      wwisLinkFor({ name: '252' })
     );
   });
 });
