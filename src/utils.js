5ac40332ac9a31cbe754011294a9c62f68f540b2
lykmapipo <lallyelias87@gmail.com>
2020-09-08 20:11:45 +0300

    feat(utils): normalize present weather

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,10 @@
-import { get as getValue, find, map, toNumber, trim } from 'lodash';
-import { isNotValue, mergeObjects } from '@lykmapipo/common';
+import { get as getValue, find, map, toNumber, trim, values } from 'lodash';
+import {
+  compact,
+  isNotValue,
+  mergeObjects,
+  parseDate,
+} from '@lykmapipo/common';
 
 import knownCities from './cities.json';
 
@@ -81,4 +86,56 @@ export const findCity = (optns) => {
 
   // return found city
   return foundCity;
+};
+
+/**
+ * @function normalizePresentForecast
+ * @name normalizePresentForecast
+ * @description Normalize present city forecast
+ * @param {object} forecast Valid present forecast
+ * @param {object} city Valid city details
+ * @returns {object} normalize present city forecast
+ * @author lally elias <lallyelias87@mail.com>
+ * @license MIT
+ * @since 0.1.0
+ * @version 0.1.0
+ * @static
+ * @public
+ * @example
+ *
+ * const forecast = { present: { '193': { cityId: 252, ... } } };
+ * const city = { cityId: 252, ... };
+ * normalizePresentForecast(forecast, city);
+ * // =>
+ */
+export const normalizePresentForecast = (forecast, city) => {
+  // collect found present forecast
+  const { present } = mergeObjects(forecast);
+  const presentForecasts = compact([].concat(values(present)));
+
+  // ensure present city
+  const presentCity = mergeObjects(city);
+
+  // find city present forecast
+  let presentForecast = find(presentForecasts, (cityForecast) => {
+    return String(cityForecast.cityId) === String(presentCity.cityId);
+  });
+
+  // normalize & convert
+  presentForecast = mergeObjects(city, {
+    date: parseDate(getValue(presentForecast, 'issue'), 'YYYYMMDDHH'),
+    weather: getValue(presentForecast, 'wxdesc'),
+    temperature: toNumber(getValue(presentForecast, 'temp')), //= > °C
+    relativeHumidity: toNumber(getValue(presentForecast, 'rh')),
+    windDirection: getValue(presentForecast, 'wd'),
+    windSpeed: (toNumber(getValue(presentForecast, 'ws')) * 18) / 5, //= > km/h
+    sunRiseAt: getValue(presentForecast, 'sunrise'),
+    sunSetAt: getValue(presentForecast, 'sunset'),
+    moonRiseAt: getValue(presentForecast, 'moonrise'),
+    moonSetAt: getValue(presentForecast, 'moonset'),
+    present: true,
+  });
+
+  // return given city present forecast
+  return presentForecast;
 };
--- test/utils.spec.js
+++ test/utils.spec.js
@@ -1,6 +1,14 @@
+import { mergeObjects } from '@lykmapipo/common';
 import { expect } from '@lykmapipo/test-helpers';
 
-import { DEFAULT_REQUEST_HEADERS, findCity } from '../src/utils';
+import {
+  DEFAULT_REQUEST_HEADERS,
+  findCity,
+  normalizePresentForecast,
+} from '../src/utils';
+
+import presentCity from './fixtures/city.json';
+import presentForecast from './fixtures/present.json';
 
 describe('utils', () => {
   it('should provide default request headers', () => {
@@ -28,5 +36,28 @@ describe('utils', () => {
     expect(findCity({ name: 'Dar Es Salaam' })).to.be.eql(
       findCity({ name: 252 })
     );
+  });
+
+  it('should normalize present city forecats', () => {
+    const city = mergeObjects(presentCity);
+    const forecast = mergeObjects(presentForecast);
+
+    const normalizedForecast = normalizePresentForecast(forecast, city);
+    expect(normalizedForecast).to.exist.and.be.an('object');
+    expect(normalizedForecast.country).to.exist.and.be.a('string');
+    expect(normalizedForecast.city).to.exist.and.be.a('string');
+    expect(normalizedForecast.cityId).to.exist.and.be.a('number');
+    expect(normalizedForecast.date).to.exist.and.be.a('date');
+    expect(normalizedForecast.weather).to.exist.and.be.a('string');
+    expect(normalizedForecast.temperature).to.exist.and.be.a('number');
+    expect(normalizedForecast.relativeHumidity).to.exist.and.be.a('number');
+    // expect(normalizedForecast.pressure).to.exist.and.be.a('number');
+    expect(normalizedForecast.windDirection).to.exist.and.be.a('string');
+    expect(normalizedForecast.windSpeed).to.exist.and.be.a('number');
+    expect(normalizedForecast.sunRiseAt).to.exist.and.be.a('string');
+    expect(normalizedForecast.sunSetAt).to.exist.and.be.a('string');
+    // expect(normalizedForecast.moonRiseAt).to.exist.and.be.a('string');
+    // expect(normalizedForecast.moonSetAt).to.exist.and.be.a('string');
+    expect(normalizedForecast.present).to.be.true;
   });
 });
